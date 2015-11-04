329691b5d07c741b2a95d7eeef651e84e14b0928
DavidMFlynn <dflynn.oxfordvue@gmail.com>
2015-11-03 16:51:18 -0800

    Sample Code
    A simple I2C test program.

---
+++ TestI2CMaster.X/build/default/production/_ext/1822053830/TestI2CMaster.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1822053830/TestI2CMaster.o:  \
+	/Projects/I2CTest/TestI2CMaster.asm  \
+	/Projects/I2CTest/p12f1822.inc  \
+	/Projects/I2CTest/F1822_Macros.inc  \
+	/Projects/I2CTest/I2C_MASTER.inc  \
+	/Projects/I2CTest/F1822_Common.inc  \
+
---
+++ TestI2CSlave.X/build/default/production/_ext/1822053830/TestI2CSlave.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1822053830/TestI2CSlave.o:  \
+	/Projects/I2CTest/TestI2CSlave.asm  \
+	/Projects/I2CTest/p12f1822.inc  \
+	/Projects/I2CTest/F1822_Macros.inc  \
+	/Projects/I2CTest/I2C_SLAVE.inc  \
+	/Projects/I2CTest/F1822_Common.inc  \
+
